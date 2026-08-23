3c4daea75c727518bbac010c0be6d9ac7de09b9d
rosezaino801 <rosezaino801@gmail.com>
2026-08-23 18:34:59 +0100

    Create workers.js

---
+++ workers.js
@@ -0,0 +1,28 @@
+export default {
+  async fetch(request, env) {
+    const url =
+      "https://api.the-odds-api.com/v4/sports/basketball_nba/odds/" +
+      "?regions=us&markets=h2h,spreads,totals&oddsFormat=decimal&apiKey=" +
+      env.ODDS_API_KEY;
+
+    try {
+      const response = await fetch(url);
+      const data = await response.text();
+
+      return new Response(data, {
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*"
+        }
+      });
+    } catch (error) {
+      return new Response(
+        JSON.stringify({ error: error.message }),
+        {
+          status: 500,
+          headers: { "Content-Type": "application/json" }
+        }
+      );
+    }
+  }
+};
